b0cd74be782b796ca368f0215006ea740cca3ae4
maof97 <48106483+maof97@users.noreply.github.com>
2023-03-18 20:18:14 +0100

    Finished daemon and core scripts + worker logik

---
+++ zsoar_worker.py
@@ -0,0 +1,189 @@
+# Z-SOAR
+# Created by: Martin Offermann
+# This module is the worker script that handles the main logic of the Z-SOAR project.
+#
+# The main logic is as follows:
+#
+# - Loop through every installed integration for getting new detection alerts
+# - Loop through each of the detections and check if any playbook is able to handle it
+# - - If a playbook is able to handle the detection, it will be executed
+# - - If all playbooks are executed, the next detection will be checked
+# (Playbooks decide if a detection is a false positive or not and what action should be taken. A playbook can and should make use of the libraries and integrations provided by Z-SOAR.)
+# - If no playbook is able to handle the detection, it will be logged and the next detection will be checked
+
+import os
+import sys
+import time
+
+import lib.config_helper as config_helper
+import lib.logging_helper as logging_helper
+import lib.class_helper as class_helper  # TODO: Implement class_helper.py
+
+
+def main(config, fromDaemon=False):
+    """Main function of the worker script.
+
+    Args:
+        config (dict): The config dictionary
+        fromDaemon (bool): If the script was called from the daemon
+
+    Returns:
+        None
+    """
+    # Get the logger
+    mlog = logging_helper.Log("zsoar_worker")
+
+    # Get every installed integration from config
+    integrations = config["integrations"]  # TODO: Implement this in config_helper.py
+
+    DetectionArray = []
+    DetectionReportArray = []
+
+    for integration in integrations:
+        module_name = integration["name"]
+
+        # Check if the module is enabled
+        if not integration["enabled"]:
+            mlog.warning("The module " + module_name + " is disabled. Skipping.")
+            continue
+
+        # Check if the module exists
+        if not class_helper.check_module_exists(module_name):
+            mlog.error("The module " + module_name + " does not exist. Skipping.")
+            continue
+
+        # Check if module provides getting new detections
+        if not class_helper.check_module_has_function(module_name, "zs_provide_new_detections"):
+            mlog.debug(
+                "The module "
+                + module_name
+                + " does not provide the function zs_provide_new_detections. Skipping."
+            )
+            continue
+
+        # Make the actual call to the integration
+        try:
+            mlog.info("Calling module " + module_name)
+            new_detections = module_name.zs_provide_new_detections()
+        except Exception as e:
+            mlog.error(
+                "The module " + module_name + " failed to provide new detections. Error: " + str(e)
+            )
+            continue
+
+        # Check if the module provided any detections
+        if not new_detections:
+            mlog.info("The module " + module_name + " did not provide any detections.")
+            continue
+        else:
+            mlog.info(
+                "The module "
+                + module_name
+                + " provided "
+                + str(len(new_detections))
+                + " new detections."
+            )
+
+        # Check if the detections are valid and add them to the array
+        for detection in new_detections:
+            if not isinstance(detection, class_helper.Detection):
+                mlog.warning(
+                    "The module " + module_name + " provided an invalid detection. Skipping."
+                )
+            else:
+                mlog.inf(
+                    "Adding new detection "
+                    + detection.get_title()
+                    + " ("
+                    + detection.get_id()
+                    + ") to the detection array."
+                )
+                DetectionArray.append(detection)
+
+    # Loop through each detection
+    for detection_report in DetectionArray:
+        detection_title = detection_report.get_title()
+        detection_id = detection_report.get_id()
+        detectionHandled = False
+
+        # Check every playbook if it can handle the detection
+        for playbook in config["playbooks"]:
+            playbook_name = playbook["name"]
+
+            # Check if the playbook is enabled
+            if not playbook["enabled"]:
+                mlog.warning("The playbook " + playbook_name + " is disabled. Skipping.")
+                continue
+
+            # Check if the playbook exists
+            if class_helper.check_module_exists(playbook_name):
+                mlog.error("The playbook " + playbook_name + " does not exist. Skipping.")
+                continue
+
+            # Ask the playbook if it can handle the detection
+            try:
+                mlog.info(
+                    f"Calling playbook {playbook_name} to check if it can handle current detection '{detection_title}' ({detection_id})"
+                )
+                can_handle = playbook_name.zs_can_handle_detection(detection_report)
+            except Exception as e:
+                mlog.warning(
+                    "The playbook "
+                    + playbook_name
+                    + " failed to check if it can handle the detection. Error: "
+                    + str(e)
+                )
+                continue
+
+            # Let the playbook handle the detection
+            if can_handle:
+                try:
+                    mlog.info(
+                        f"Calling playbook to handle the current detection {detection_title} ({detection_id})"
+                    )
+                    detection_report = playbook_name.zs_handle_detection(detection_report)
+                except Exception as e:
+                    mlog.warning(
+                        "The playbook "
+                        + playbook_name
+                        + " failed to handle the detection. Error: "
+                        + str(e)
+                    )
+                    continue
+
+                # Check if the playbook handled the detection correctly
+                if not isinstance(detection_report, class_helper.DetectionReport):
+                    mlog.error(
+                        "The playbook "
+                        + playbook_name
+                        + " did not return a valid detection report. Skipping."
+                    )
+                    continue
+                else:
+                    mlog.info("The playbook " + playbook_name + " handled the detection correctly.")
+                    detectionHandled = True
+
+                # Add the detection report to the detectior report array
+                mlog.info(
+                    f"Adding detection report for detection {detection_title} ({detection_id}) to the detection report array."
+                )
+                DetectionReportArray.append(detection_report)
+
+                # Break the loop to check the next detection
+                break
+
+        # If no playbook was able to handle the detection, log it
+        if not detectionHandled:
+            mlog.warning(
+                "No playbook was able to handle the detection "
+                + detection_title
+                + " ("
+                + detection_id
+                + ")."
+            )
+
+    mlog.info("Finished worker script.")
+
+
+if __name__ == "__main__":
+    main()
